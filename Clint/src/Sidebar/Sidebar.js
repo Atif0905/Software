e2a1088883055a6cf9afc74394153e6bf4ecc3d6
VSinghwal <vedantsinghwal@gmail.com>
2024-02-13 10:38:39 +0530

    13thfeb2024

--- Clint/src/Sidebar/Sidebar.js
+++ Clint/src/Sidebar/Sidebar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faUser, faClipboard  } from '@fortawesome/free-regular-svg-icons';
-import { faRightFromBracket, faChalkboardUser } from '@fortawesome/free-solid-svg-icons';
+import {  faUser, faClipboard } from '@fortawesome/free-regular-svg-icons';
+import { faRightFromBracket, faListCheck ,faChalkboardUser } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
@@ -23,9 +23,9 @@ const Sidebar = () => {
         <img src='./grouplogo.png' className='logoo' alt=''/>
         <div className='mt-2'>
           <li className='Links'><Link to="/"><FontAwesomeIcon icon={faChalkboardUser} className='mar'/>Dashboard</Link></li>
-          {/* <li className='Links'><Link to="/Projects"><FontAwesomeIcon icon={faListCheck} className='mar' />Projects</Link></li> */}
-          {/* <li className='Links'><Link to="/reports"><FontAwesomeIcon icon={faClipboard} className='mar' />Reports</Link></li> */}
-          {/* <li className='Links'><Link to="/user"><FontAwesomeIcon icon={faUser} className='mar'/>User</Link></li> */}
+          <li className='Links'><Link to="/Projects"><FontAwesomeIcon icon={faListCheck} className='mar' />Projects</Link></li>
+          <li className='Links'><Link to="/reports"><FontAwesomeIcon icon={faClipboard} className='mar' />Reports</Link></li>
+          <li className='Links'><Link to="/Adminuser"><FontAwesomeIcon icon={faUser} className='mar'/>User</Link></li>
           <li className='Links' onClick={logOut}><Link ><FontAwesomeIcon icon={faRightFromBracket} className='mar'/>Logout</Link></li>
         </div>
       </ul>
