43d56eb18ba4aec83d708d5c9a8fb4d18f7a2a5a
Atif Naquee <atifnaquee09@gmial.com>
2024-02-19 21:11:21 +0530

    hg

--- Clint/src/Home/AdminDashboard/ViewCastumer.js
+++ Clint/src/Home/AdminDashboard/ViewCastumer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './AdminDashboard.css'
+
 const CustomerList = () => {
   const [customers, setCustomers] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -11,9 +12,11 @@ const CustomerList = () => {
       try {
         const response = await axios.get(`${process.env.REACT_APP_API_URL}/customers`);
         const customersWithProjectNames = await Promise.all(response.data.map(async (customer) => {
-          const projectName = await fetchProjectName(customer.project);
-          const blockName = await fetchBlockName(customer.project, customer.block);
-          const unitName = await fetchUnitName(customer.project, customer.block, customer.plotOrUnit);
+          const [projectName, blockName, unitName] = await Promise.all([
+            fetchProjectName(customer.project),
+            fetchBlockName(customer.project, customer.block),
+            fetchUnitName(customer.project, customer.block, customer.plotOrUnit)
+          ]);
           return { ...customer, projectName, blockName, unitName };
         }));
         setCustomers(customersWithProjectNames);
@@ -28,37 +31,29 @@ const CustomerList = () => {
     fetchCustomers();
   }, []);
 
-  const fetchProjectName = async (projectId) => {
+  const fetchData = async (url) => {
     try {
-      if (!projectId) throw new Error('Project ID is not defined');
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}/getProject/${projectId}`);
+      const response = await axios.get(url);
       return response.data.data.name;
     } catch (error) {
-      console.error(`Error fetching project name for ID ${projectId}:`, error);
-      return 'Unknown Project';
+      console.error('Error fetching data:', error);
+      return 'Unknown';
     }
+  };
+
+  const fetchProjectName = async (projectId) => {
+    if (!projectId) return 'Unknown Project';
+    return fetchData(`${process.env.REACT_APP_API_URL}/getProject/${projectId}`);
   };
 
   const fetchBlockName = async (projectId, blockId) => {
-    try {
-      if (!projectId || !blockId) throw new Error('Project ID or Block ID is not defined');
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}/getBlock/${projectId}/${blockId}`);
-      return response.data.data.name;
-    } catch (error) {
-      console.error(`Error fetching block name for ID ${blockId} in project ${projectId}:`, error);
-      return 'Unknown Block';
-    }
+    if (!projectId || !blockId) return 'Unknown Block';
+    return fetchData(`${process.env.REACT_APP_API_URL}/getBlock/${projectId}/${blockId}`);
   };
 
   const fetchUnitName = async (projectId, blockId, unitId) => {
-    try {
-      if (!projectId || !blockId || !unitId) throw new Error('Project ID, Block ID, or Unit ID is not defined');
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}/getUnit/${projectId}/${blockId}/${unitId}`);
-      return response.data.data.name;
-    } catch (error) {
-      console.error(`Error fetching unit name for ID ${unitId} in block ${blockId} of project ${projectId}:`, error);
-      return 'Unknown Unit';
-    }
+    if (!projectId || !blockId || !unitId) return 'Unknown Unit';
+    return fetchData(`${process.env.REACT_APP_API_URL}/getUnit/${projectId}/${blockId}/${unitId}`);
   };
 
   if (loading) {
@@ -72,51 +67,51 @@ const CustomerList = () => {
   return (
     <div className='main-content'>
       <h2>Customer List</h2>
-      <div class="table-container">
-      <table id="myTable">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Father/Husband Name</th>
-            <th>Address</th>
-            <th>Aadhar Number</th>
-            <th>PAN Number</th>
-            <th>Mobile Number</th>
-            <th>Email</th>
-            <th>Property Type</th>
-            <th>Project</th>
-            <th>Block</th>
-            <th>Plot/Unit</th>
-            <th>Discount</th>
-            <th>Payment Plan</th>
-            <th>Booking Date</th>
-            <th>Booking Type</th>
-            <th>Send Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map((customer, index) => (
-            <tr key={index}>
-              <td>{customer.name}</td>
-              <td>{customer.fatherOrHusbandName}</td>
-              <td>{customer.address}</td>
-              <td>{customer.aadharNumber}</td>
-              <td>{customer.panNumber}</td>
-              <td>{customer.mobileNumber}</td>
-              <td>{customer.email}</td>
-              <td>{customer.propertyType}</td>
-              <td>{customer.projectName}</td>
-              <td>{customer.blockName}</td>
-              <td>{customer.unitName}</td>
-              <td>{customer.discount}</td>
-              <td>{customer.paymentPlan}</td>
-              <td>{customer.bookingDate}</td>
-              <td>{customer.bookingType}</td>
-              <td>{customer.sendEmail ? 'Yes' : 'No'}</td>
+      <div className="table-container">
+        <table id="myTable">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Father/Husband Name</th>
+              <th>Address</th>
+              <th>Aadhar Number</th>
+              <th>PAN Number</th>
+              <th>Mobile Number</th>
+              <th>Email</th>
+              <th>Property Type</th>
+              <th>Project</th>
+              <th>Block</th>
+              <th>Plot/Unit</th>
+              <th>Discount</th>
+              <th>Payment Plan</th>
+              <th>Booking Date</th>
+              <th>Booking Type</th>
+              <th>Send Email</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {customers.map((customer, index) => (
+              <tr key={index}>
+                <td>{customer.name}</td>
+                <td>{customer.fatherOrHusbandName}</td>
+                <td>{customer.address}</td>
+                <td>{customer.aadharNumber}</td>
+                <td>{customer.panNumber}</td>
+                <td>{customer.mobileNumber}</td>
+                <td>{customer.email}</td>
+                <td>{customer.propertyType}</td>
+                <td>{customer.projectName}</td>
+                <td>{customer.blockName}</td>
+                <td>{customer.unitName}</td>
+                <td>{customer.discount}</td>
+                <td>{customer.paymentPlan}</td>
+                <td>{customer.bookingDate}</td>
+                <td>{customer.bookingType}</td>
+                <td>{customer.sendEmail ? 'Yes' : 'No'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
