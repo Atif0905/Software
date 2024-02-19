37309f0abb6b493cd817a81921616a6ada09e17f
VSinghwal <vedantsinghwal@gmail.com>
2024-02-19 18:23:17 +0530

    customer form edted

--- Clint/src/Home/AdminDashboard/Uploads/Coustmer.js
+++ Clint/src/Home/AdminDashboard/Uploads/Coustmer.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './Customer.css'
 
 const AddCustomerForm = () => {
+  
   const [formData, setFormData] = useState({
     name: '',
     fatherOrHusbandName: '',
@@ -26,7 +27,12 @@ const AddCustomerForm = () => {
   const [projects, setProjects] = useState([]);
   const [showBlocks, setShowBlocks] = useState(false);
   const [showUnits, setShowUnits] = useState(false);
-
+  const [plotSize, setPlotSize] = useState('');
+  const [sizeType, setSizeType] = useState('');
+  const [rate, setRate] = useState('');
+  const [idcCharges, setIdcCharges] = useState('');
+  const [plcCharges, setPlcCharges] = useState('');
+  const [selectedBlockUnits, setSelectedBlockUnits] = useState([]);
   useEffect(() => {
     fetchProjects();
   }, []);
@@ -58,7 +64,7 @@ const AddCustomerForm = () => {
       console.error("Error fetching projects:", error);
     }
   };
-
+  
   const getUnitCount = async (projectId, blockId) => {
     try {
       const response = await axios.get(
@@ -100,63 +106,126 @@ const AddCustomerForm = () => {
       const selectedProject = projects.find(project => project._id === formData.selectedProjectId);
       const selectedBlock = selectedProject?.blocks.find(block => block._id === formData.selectedBlockId);
       const selectedUnit = selectedBlock?.units.find(unit => unit._id === formData.selectedUnitId);
-
+  
       const dataToSend = {
         ...parsedFormData,
         selectedProject,
         selectedBlock,
         selectedUnit
       };
-
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/addCustomer`, dataToSend);
+  
+      // Add a PUT request to mark the unit as sold
+      const markUnitSoldResponse = await axios.put(`${process.env.REACT_APP_API_URL}/markUnitSold/${selectedProject._id}/${selectedBlock._id}/${selectedUnit._id}`);
       
-      if (response.status === 201) {
-        console.log('Customer added successfully:', response.data);
-        setFormData({
-          name: '',
-          fatherOrHusbandName: '',
-          address: '',
-          aadharNumber: '',
-          panNumber: '',
-          mobileNumber: '',
-          income: '',
-          email: '',
-          propertyType: '',
-          selectedProjectId: '',
-          selectedBlockId: '',
-          selectedUnitId: '',
-          discount: '',
-          paymentPlan: '',
-          bookingDate: '',
-          bookingType: '',
-          sendEmail: false
-        });
+      // If the unit is successfully marked as sold, proceed with adding the customer
+      if (markUnitSoldResponse.status === 200 && markUnitSoldResponse.data.status === 'ok') {
+        const addCustomerResponse = await axios.post(`${process.env.REACT_APP_API_URL}/addCustomer`, dataToSend);
+        
+        if (addCustomerResponse.status === 201) {
+          console.log('Customer added successfully:', addCustomerResponse.data);
+          alert('Customer Added Successfully');
+          // Reset form data
+          setFormData({
+            name: '',
+            fatherOrHusbandName: '',
+            address: '',
+            aadharNumber: '',
+            panNumber: '',
+            mobileNumber: '',
+            income: '',
+            email: '',
+            propertyType: '',
+            selectedProjectId: '',
+            selectedBlockId: '',
+            selectedUnitId: '',
+            discount: '',
+            paymentPlan: '',
+            bookingDate: '',
+            bookingType: '',
+            sendEmail: false,
+            plotSize : '',
+            rate : '',
+            idcCharges : '',
+            plcCharges : ''
+          });
+        } else {
+          console.error('Failed to add customer:', addCustomerResponse.statusText);
+        }
       } else {
-        console.error('Failed to add customer:', response.statusText);
+        console.error('Failed to mark unit as sold:', markUnitSoldResponse.statusText);
       }
     } catch (error) {
       console.error('Error adding customer:', error);
     }
   };
-
+  
+  
+  const handleClickBlock = (blockId) => {
+    setFormData({ ...formData, selectedBlockId: blockId });
+    setShowUnits(true);
+  
+    const selectedBlock = projects
+      .find(project => project._id === formData.selectedProjectId)
+      ?.blocks.find(block => block._id === blockId);
+  
+    if (selectedBlock) {
+      // Filter out units that are already booked or sold
+      const availableUnits = selectedBlock.units.filter(unit => unit.status !== 'Booked' && unit.status !== 'Sold');
+      // Update state with available units
+      setSelectedBlockUnits(availableUnits);
+    }
+  };
   const handleClickProject = (projectId) => {
     setFormData({ ...formData, selectedProjectId: projectId });
     setShowBlocks(true);
     setShowUnits(false);
   };
 
-  const handleClickBlock = (blockId) => {
-    setFormData({ ...formData, selectedBlockId: blockId });
-    setShowUnits(true);
-  };
+  
 
+  const [selectedUnitId, setSelectedUnitId] = useState('');
+
+  // const handleClickUnit = (unitId) => {
+  //   setSelectedUnitId(unitId);
+  //   setRate('');
+  //     setIdcCharges('');
+  //     setPlcCharges('');
+  // };
   const handleClickUnit = (unitId) => {
     setFormData({ ...formData, selectedUnitId: unitId });
-  };
+    const selectedUnit = projects
+      .flatMap(project => project.blocks)
+      .flatMap(block => block.units)
+      .find(unit => unit._id === unitId);
+    // Set values of plotSize, rate, idcCharges, plcCharges based on selectedUnit
+    setPlotSize(selectedUnit?.plotSize || '');
+    setRate(selectedUnit?.rate || '');
+    setIdcCharges(selectedUnit?.idcCharges || '');
+    setPlcCharges(selectedUnit?.plcCharges || '');
+  }
 
+  const handleUnitSelect = (e) => {
+    const selectedUnitId = e.target.value;
+    setSelectedUnitId(selectedUnitId);
+
+    // Find the selected unit from projects state
+    const selectedUnit = projects
+      .flatMap(project => project.blocks)
+      .flatMap(block => block.units)
+      .find(unit => unit._id === selectedUnitId);
+      const { idcCharges, plcCharges, rate, plotSize } = selectedUnit;
+    if (selectedUnit) {
+      // Update state variables with selected unit data
+      setPlotSize(selectedUnit.plotSize);
+      setSizeType(selectedUnit.sizeType);
+      setRate(selectedUnit.rate);
+      setIdcCharges(selectedUnit.idcCharges);
+      setPlcCharges(selectedUnit.plcCharges);
+    }
+  };
   return (
     <div className='main-content'>
-      <h2 >Customer Form</h2>
+      <h2 >Applicant Details</h2>
       <form onSubmit={handleSubmit}>
         <div className='d-flex justify-content-between m-3'>
         <div class="relative">
@@ -164,44 +233,64 @@ const AddCustomerForm = () => {
   <label id="label-input">Name</label>
 </div>
 <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text" name="fatherOrHusbandName" value={formData.fatherOrHusbandName} onChange={handleInputChange}/>
+  <input className="input-cal input-base" id="input" placeholder="" type="text" name="fatherOrHusbandName" value={formData.fatherOrHusbandName} onChange={handleInputChange} required />
   <label id="label-input">Fathers/Husband Name</label>
 </div>
         </div>
         <div className='d-flex justify-content-between m-3'>
         <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text" name="address" value={formData.address} onChange={handleInputChange}/>
+  <input className="input-cal input-base" id="input" placeholder="" type="text" name="address" value={formData.address} onChange={handleInputChange} required/>
   <label id="label-input">Address</label>
 </div>
 <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="number" name="aadharNumber" value={formData.aadharNumber} onChange={handleInputChange} />
+  <input className="input-cal input-base" id="input" placeholder="" type="number" name="aadharNumber" value={formData.aadharNumber} onChange={handleInputChange} required />
   <label id="label-input">Adhar Number</label>
 </div>
         </div>
         <div className='d-flex justify-content-between m-3'>
         <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text" name="panNumber" value={formData.panNumber} onChange={handleInputChange}/>
+  <input className="input-cal input-base" id="input" placeholder="" type="text" name="panNumber" value={formData.panNumber} onChange={handleInputChange} required />
   <label id="label-input">Pan Number</label>
 </div>
 <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="number" name="mobileNumber" value={formData.mobileNumber} onChange={handleInputChange}/> 
+  <input className="input-cal input-base" id="input" placeholder="" type="number" name="mobileNumber" value={formData.mobileNumber} onChange={handleInputChange} required /> 
   <label id="label-input">Mobile Number</label>
 </div>
        </div>
         <div className='d-flex justify-content-between m-3'>
         <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text" name="income" value={formData.income} onChange={handleInputChange}/>  
-  <label id="label-input">Income</label>
-</div><div className="relative">
   <input className="input-cal input-base" id="input" placeholder="" type="text" name="email" value={formData.email} onChange={handleInputChange} required/>
   <label id="label-input">Email</label>
 </div></div>
+        
+        <h2>Property Details</h2>
         <div className='d-flex justify-content-between m-3'>
         <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text" name="propertyType" value={formData.propertyType} onChange={handleInputChange}/> 
+  <input className="input-cal input-base" id="input" placeholder="" type="text" name="propertyType" value={formData.propertyType} onChange={handleInputChange} required /> 
   <label id="label-input">property Type</label>
 </div>
-        <div className="d-flex flex-wrap justify-content-between ">
+<div className="relative">
+  <input className="input-cal input-base" id="input" placeholder="" type="text" name="bookingType" value={formData.bookingType} onChange={handleInputChange}required/>
+  <label id="label-input">Booking Type</label>
+</div>
+        </div>
+        <div className='d-flex justify-content-between m-3'>
+        <div className="relative">
+  <input className="input-cal input-base" id="input" placeholder="" type="text"name="discount" value={formData.discount} onChange={handleInputChange} required />  
+  <label id="label-input">Discount</label>
+</div>
+
+<div className="relative">
+  <input className="input-cal input-base" id="input" placeholder="" type="text"name="paymentPlan" value={formData.paymentPlan} onChange={handleInputChange} required /> 
+  <label id="label-input">Payment Plan</label>
+</div>
+</div>
+        <div className='d-flex justify-content-between m-3'>
+        <div className="relative">
+  <input className="input-cal input-base" id="input" placeholder="" type="date" name="bookingDate" value={formData.bookingDate} onChange={handleInputChange} required /> 
+  <label id="label-input">Booking Date</label>
+</div>
+<div className="d-flex flex-wrap justify-content-around ">
           <select className='input-select' onChange={(e) => handleClickProject(e.target.value)}>
             <option value="">Select Project</option>
             {projects.map((project, index) => (
@@ -218,45 +307,91 @@ const AddCustomerForm = () => {
             </select>
           )}
           
-          {showUnits && formData.selectedBlockId && (
-            <select className='input-select' onChange={(e) => handleClickUnit(e.target.value)}>
-              <option value="">Select Unit</option>
-              {projects
-                .find(project => project._id === formData.selectedProjectId)
-                ?.blocks.find(block => block._id === formData.selectedBlockId)
-                ?.units.map((unit, index) => (
-                  <option key={index} value={unit._id}>{unit.name}</option>
-                ))}
-            </select>
-          )}
+          {showUnits && selectedBlockUnits.length > 0 && (
+  <select className='input-select' onChange={(e) => handleClickUnit(e.target.value)}>
+    <option value="">Select Unit</option>
+    {selectedBlockUnits
+      .filter(unit => unit.status !== "sold" && unit.status !== "hold") // Filter out sold and hold units
+      .map((unit, index) => (
+        <option key={index} value={unit._id}>{unit.name}</option>
+      ))}
+  </select>
+)}
+
+
           </div>
         </div>
         <div className='d-flex justify-content-between m-3'>
         <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text"name="discount" value={formData.discount} onChange={handleInputChange} />  
-  <label id="label-input">Discount</label>
+  <input className="input-cal input-base" id="input" placeholder="" type="text" name="income" value={formData.income} onChange={handleInputChange} required/>  
+  <label id="label-input">Income</label>
 </div>
-
-<div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text"name="paymentPlan" value={formData.paymentPlan} onChange={handleInputChange}/> 
-  <label id="label-input">Payment Plan</label>
-</div></div>
-        <div className='d-flex justify-content-between m-3'>
-        <div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="date" name="bookingDate" value={formData.bookingDate} onChange={handleInputChange}/> 
-  <label id="label-input">Booking Date</label>
-</div><div className="relative">
-  <input className="input-cal input-base" id="input" placeholder="" type="text" name="bookingType" value={formData.bookingType} onChange={handleInputChange}required/>
-  <label id="label-input">Booking Type</label>
-</div>
+        <div className="container">
+      <input type="checkbox" id="cbx2" name="sendEmail"  checked={formData.sendEmail} onChange={handleCheckboxChange} style={{ display: 'none' }} required />
+      <label htmlFor="cbx2" className="check">
+        <svg width="18px" height="18px" viewBox="0 0 18 18">
+          <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+          <polyline points="1 9 7 14 15 4"></polyline>
+        </svg>
+      </label>
+      Send Email
+    </div>
+    </div>
+    <div className='d-flex justify-content-between m-3'>
+          <div className='relative'>
+            <input
+              type="number"
+              className="input-cal input-base"
+              id="input"
+              placeholder=""
+              value={plotSize}
+              onChange={(e) => setPlotSize(e.target.value)}
+              required
+            />
+            <label id="label-input">Plot size</label>
+          </div>
+          <div className='relative'>
+            <input
+              type="number"
+              className="input-cal input-base"
+              id="input"
+              placeholder=""
+              value={rate}
+              onChange={(e) => setRate(e.target.value)}
+              required
+            />
+            <label id="label-input">Rate</label>
+          </div>
         </div>
-        <label className='imput-name m-3'>
-          <input type="checkbox" name="sendEmail"  checked={formData.sendEmail} onChange={handleCheckboxChange} />
-          Send Email
-        </label>
+        <div className='d-flex justify-content-between m-3'>
+          <div className='relative'>
+            <input
+              type="number"
+              className="input-cal input-base"
+              id="input"
+              placeholder=""
+              value={idcCharges}
+              onChange={(e) => setIdcCharges(e.target.value)}
+              required
+            />
+            <label id="label-input">IDC Charges</label>
+          </div>
+          <div className='relative'>
+            <input
+              type="number"
+              className="input-cal input-base"
+              id="input"
+              placeholder=""
+              value={plcCharges}
+              onChange={(e) => setPlcCharges(e.target.value)}
+              required
+            />
+            <label id="label-input">PLC Charges</label>
+          </div>
+        </div>
+    
         <div className='d-flex justify-content-center'>
         <button type="submit" className='submitbutton'>Submit</button></div>
-        
       </form>
     </div>
   );
